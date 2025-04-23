16f051404d8448cf571dd77e0eaab3b6cba924d9
Edwin-Aguirre <edwina3@uci.edu>
2025-04-23 14:01:42 -0700

    Project Completed

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,6 +1,13 @@
 extends Control
 
 
+@onready var play_button: UIButton = $MC/VB/PlayButton
+
+
+func _ready():
+	play_button.grab_focus()
+
+
 func _on_play_button_pressed() -> void:
 	GameManager.load_level_scene()
 
